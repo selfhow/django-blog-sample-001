9c86814de346c50815b19cac10ba80945616a84f
selfhow <selfhowcom@gmail.com>
2018-04-10 15:10:06 +0900

    venv 제외

--- src/venv35/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\_my\git\selfhow\django-blog-sample-001\src\venv35\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
